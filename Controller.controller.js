e6b426e0b93efac26df455497caa1fadcf4489cb
s4180t <s4180t@yandex.ru>
2021-07-15 18:07:40 +0300

    It Works!

--- Controller.controller.js
+++ Controller.controller.js
@@ -28,6 +28,39 @@ sap.ui.define([
 		onExpandSelection: function() {
 			var oTreeTable = this.byId("TreeTableBasic");
 			oTreeTable.expand(oTreeTable.getSelectedIndices());
+		},
+
+		onTreeTableBindingChange: function (oEvent) {
+			var oBinding = oEvent.getSource();
+			var aSavedExpanded = JSON.parse(localStorage.getItem("expandedItems") || "[]");
+			var bCollapseOrExpand = oEvent.getParameter("reason") === "collapse" || oEvent.getParameter("reason") === "expand";
+
+			if (!bCollapseOrExpand && !oBinding.bSkip && oBinding.getLength && oBinding.getLength()) {
+				for (var index = 0; index < aSavedExpanded.length; index++) {
+					var sExpandedGroup = aSavedExpanded[index];
+
+					oBinding._updateTreeState({
+						groupID: sExpandedGroup,
+						expanded: true
+					});
+				}
+
+				oBinding.bSkip = true;
+				oBinding.refresh();
+				setTimeout(function () {
+					delete oBinding.bSkip;
+				});
+			}
+			if (bCollapseOrExpand) {
+				setTimeout(function () {
+					localStorage.setItem("expandedItems", JSON.stringify(oBinding.getCurrentTreeState()._getExpandedList().split(";")));
+				});
+			}
+		},
+
+		onExpandFirstTwoLevels: function () {
+			var oTreeTable = this.byId("TreeTableBasic");
+			oTreeTable.expandToLevel(2);
 		}
 	});
 });
